85d8b070c7f0a38d4421ba42141e7c07d15584d0
souf1neCoder <m'channa.soufiane@ofppt-edu.ma>
2022-03-19 03:08:53 +0100

    remove errors from main js

--- main.js
+++ main.js
@@ -1,16 +1,1 @@
-function uniteUnique(arr) {
-  let myarr = [...arguments]
-  let res = [];
-  for (let i = 0; i < myarr.length; i++) {
-    for (let j = 0; j < myarr[i].length; j++) {
-      if(!res.includes(myarr[i][j])){
-        res.push(myarr[i][j])
-      }
-      
-    }
-    
-  }
-  console.log(res);
-}
-
-console.log(uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]));
+alert("hi from git");
